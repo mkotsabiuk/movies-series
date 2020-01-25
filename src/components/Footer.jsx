0f3dbec3cd184d56527254272480db16766812e3
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2020-01-25 03:09:23 +0200

    Added footer and navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            Developed by: &nbsp;
+            <span className="text-warning font-weight-normal">
+              Mykola Kotsabiuk
+            </span>
+            , Using <i className="fab fa-react" /> React JS &amp; Redux JS
+            integrated with external movies data API &nbsp;
+            <a href="http://www.omdbapi.com/" target="_blank">
+              OMDB
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
