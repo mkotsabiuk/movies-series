1a1f5eef110675142c7009dc1de2129d473827f7
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2020-05-12 20:55:52 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/home/Landing";
+import Movie from "./components/home/Movie";
 import store from "./store";
 import { Provider } from "react-redux";
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Provider store={store}>
-      <Navbar />
-      <Landing />
-      <Footer />
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/movie/:id" component={Movie}/>
+          <Footer />
+        </div>
+      </Router>
     </Provider>
   );
 }
