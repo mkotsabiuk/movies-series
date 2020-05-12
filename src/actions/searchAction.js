51b597b9ea3a23db484258b3907b5913a539b059
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2020-05-12 20:54:32 +0200

    Add actions

--- src/actions/searchAction.js
+++ src/actions/searchAction.js
@@ -1,7 +1,6 @@
-import { SEARCH_MOVIE, FETCH_MOVIES } from "./types";
+import { SEARCH_MOVIE, FETCH_MOVIES, FETCH_MOVIE, LOADING } from "./types";
 import axios from "axios";
 import { ApiKey } from "../APIKey";
-import { compose } from "redux";
 
 export const searchMovie = text => dispatch => {
   dispatch({
@@ -14,11 +13,27 @@ export const fetchMovies = text => dispatch => {
   axios
     .get(`http://www.omdbapi.com/?apikey=${ApiKey}&s=${text}`)
     .then(response =>
-      // console.log(response);
       dispatch({
         type: FETCH_MOVIES,
         payload: response.data.Search
       })
     )
     .catch(err => console.log(err));
+};
+
+export const fetchMovie = id => dispatch => {
+  axios.get(`http://www.omdbapi.com/?apikey=${ApiKey}&i=${id}`)
+  .then(response =>
+    dispatch({
+      type: FETCH_MOVIE,
+      payload: response.data
+    })
+  )
+  .catch(err => console.log(err));
+};
+
+export const setLoading = () => dispatch => {
+  dispatch({
+    type: LOADING
+  })
 };
