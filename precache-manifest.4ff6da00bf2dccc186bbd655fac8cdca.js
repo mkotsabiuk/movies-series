db47b5484cc69e2612cd1918edf79997a1899edc
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2020-05-12 21:41:16 +0200

    Updates

---
+++ precache-manifest.4ff6da00bf2dccc186bbd655fac8cdca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4e9d4113fec25b8a57564aa5dd63ac",
+    "url": "/movies-series/index.html"
+  },
+  {
+    "revision": "b100ccc13e8979b7ef78",
+    "url": "/movies-series/static/css/main.f3a154a4.chunk.css"
+  },
+  {
+    "revision": "fd74364b8f96099c1b8c",
+    "url": "/movies-series/static/js/2.0f639367.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movies-series/static/js/2.0f639367.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b100ccc13e8979b7ef78",
+    "url": "/movies-series/static/js/main.5d8a7bd2.chunk.js"
+  },
+  {
+    "revision": "4db90948f8ff3b4802c9",
+    "url": "/movies-series/static/js/runtime-main.72625179.js"
+  }
+]);
